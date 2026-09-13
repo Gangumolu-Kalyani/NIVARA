1769ba4725d6efc7b3b3f7bc0af47dba9d79f7d0
24wh1a0516-crypto <24wh1a0516-crypto@users.noreply.github.com>
2026-09-13 18:08:13 +0530

    first springboot version

---
+++ nivara-backend/src/main/java/com/sih/nivara/NivaraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sih.nivara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NivaraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NivaraBackendApplication.class, args);
+	}
+
+}
---
+++ nivara-backend/src/test/java/com/sih/nivara/NivaraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.nivara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NivaraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
